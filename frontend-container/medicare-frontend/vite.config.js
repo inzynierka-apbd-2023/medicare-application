07e1f7e20d43237fc4643a3e2f3224d03db74cc7
Hubert Sienicki <businesssienicki@gmail.com>
2025-05-06 18:18:46 +0200

    add axios for api and tailwind for styling

--- frontend-container/medicare-frontend/vite.config.js
+++ frontend-container/medicare-frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import postcss from 'vite-plugin-postcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    postcss({
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    }),
+  ],
 })
