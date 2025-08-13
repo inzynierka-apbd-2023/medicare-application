12c6489272f3b72bb8e00079533702adb5e02e74
Hubert Sienicki <businesssienicki@gmail.com>
2025-08-13 23:53:15 +0200

    fix(catalog): add Description column to catalog.release to unblock diagnostics

--- backend-container/MedicalCatalogService/MedicalCatalogService/Migrations/MedicalCatalogDbContextModelSnapshot.cs
+++ backend-container/MedicalCatalogService/MedicalCatalogService/Migrations/MedicalCatalogDbContextModelSnapshot.cs
@@ -16,82 +16,283 @@ namespace MedicalCatalogService.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
 
-            modelBuilder.Entity("MedicalCatalogService.Models.MedicalCondition", b =>
+            modelBuilder.Entity("MedicalCatalogService.Models.Icd10", b =>
             {
-                b.Property<string>("Id")
-                    .HasMaxLength(36)
-                    .HasColumnType("nvarchar(36)")
-                    .HasDefaultValueSql("CONVERT(VARCHAR(36), NEWID())");
-
                 b.Property<string>("Code")
-                    .IsRequired()
+                    .HasMaxLength(10)
+                    .HasColumnType("nvarchar(10)");
+
+                b.Property<DateTime?>("EffectiveFrom")
+                    .HasColumnType("datetime2");
+
+                b.Property<DateTime?>("EffectiveTo")
+                    .HasColumnType("datetime2");
+
+                b.Property<string>("Status")
                     .HasMaxLength(50)
                     .HasColumnType("nvarchar(50)");
 
-                b.Property<string>("Description")
-                    .HasMaxLength(1000)
-                    .HasColumnType("nvarchar(1000)");
-
-                b.Property<bool>("IsActive")
-                    .HasColumnType("bit")
-                    .HasDefaultValue(true);
-
-                b.Property<string>("Name")
+                b.Property<string>("Title")
                     .IsRequired()
-                    .HasMaxLength(200)
-                    .HasColumnType("nvarchar(200)");
+                    .HasMaxLength(500)
+                    .HasColumnType("nvarchar(500)");
 
-                b.Property<DateTime>("UpdatedAt")
-                    .HasColumnType("datetime2")
-                    .HasDefaultValueSql("SYSUTCDATETIME()");
+                b.HasKey("Code");
 
-                b.HasKey("Id");
+                b.HasIndex("Title");
 
-                b.HasIndex("Code")
-                    .IsUnique();
-
-                b.ToTable("Medical_Condition", "catalog");
+                b.ToTable("icd10", "catalog");
             });
 
-            modelBuilder.Entity("MedicalCatalogService.Models.LabTestType", b =>
+            modelBuilder.Entity("MedicalCatalogService.Models.LoincEntry", b =>
             {
-                b.Property<string>("Id")
-                    .HasMaxLength(36)
-                    .HasColumnType("nvarchar(36)")
-                    .HasDefaultValueSql("CONVERT(VARCHAR(36), NEWID())");
+                b.Property<string>("LoincNum")
+                    .HasMaxLength(20)
+                    .HasColumnType("nvarchar(20)");
 
-                b.Property<string>("Code")
+                b.Property<string>("Component")
+                    .HasMaxLength(255)
+                    .HasColumnType("nvarchar(255)");
+
+                b.Property<string>("LongCommonName")
+                    .HasMaxLength(500)
+                    .HasColumnType("nvarchar(500)");
+
+                b.Property<string>("MethodTyp")
+                    .HasMaxLength(100)
+                    .HasColumnType("nvarchar(100)");
+
+                b.Property<string>("Property")
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
+
+                b.Property<string>("ScaleTyp")
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
+
+                b.Property<string>("System")
+                    .HasMaxLength(100)
+                    .HasColumnType("nvarchar(100)");
+
+                b.Property<string>("TimeAspct")
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
+
+                b.HasKey("LoincNum");
+
+                b.HasIndex("Component");
+
+                b.HasIndex("LongCommonName");
+
+                b.ToTable("loinc", "catalog");
+            });
+
+            modelBuilder.Entity("MedicalCatalogService.Models.CatalogRelease", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                b.Property<DateTime>("ReleasedOn")
+                    .HasColumnType("datetime2");
+
+                b.Property<string>("System")
                     .IsRequired()
                     .HasMaxLength(50)
                     .HasColumnType("nvarchar(50)");
 
-                b.Property<bool>("IsActive")
-                    .HasColumnType("bit")
-                    .HasDefaultValue(true);
-
-                b.Property<string>("Name")
+                b.Property<string>("Version")
                     .IsRequired()
-                    .HasMaxLength(200)
-                    .HasColumnType("nvarchar(200)");
-
-                b.Property<string>("ReferenceRange")
-                    .HasMaxLength(200)
-                    .HasColumnType("nvarchar(200)");
-
-                b.Property<string>("Unit")
-                    .HasMaxLength(50)
-                    .HasColumnType("nvarchar(50)");
-
-                b.Property<DateTime>("UpdatedAt")
-                    .HasColumnType("datetime2")
-                    .HasDefaultValueSql("SYSUTCDATETIME()");
+                    .HasMaxLength(100)
+                    .HasColumnType("nvarchar(100)");
 
                 b.HasKey("Id");
 
-                b.HasIndex("Code")
+                b.HasIndex("System", "Version")
                     .IsUnique();
 
-                b.ToTable("Lab_Test_Type", "catalog");
+                b.ToTable("release", "catalog");
+            });
+
+            modelBuilder.Entity("MedicalCatalogService.Models.LoincMapTo", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                b.Property<string>("Comment")
+                    .HasMaxLength(500)
+                    .HasColumnType("nvarchar(500)");
+
+                b.Property<string>("MapToScaleTyp")
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
+
+                b.Property<string>("SourceLoinc")
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .HasColumnType("nvarchar(20)");
+
+                b.Property<string>("Status")
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
+
+                b.Property<string>("TargetLoinc")
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .HasColumnType("nvarchar(20)");
+
+                b.HasKey("Id");
+
+                b.HasIndex("SourceLoinc", "TargetLoinc")
+                    .IsUnique();
+
+                b.ToTable("loinc_map_to", "catalog");
+            });
+
+            modelBuilder.Entity("MedicalCatalogService.Models.LoincAnswerList", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                b.Property<string>("AnswerId")
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
+
+                b.Property<string>("AnswerListId")
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
+
+                b.Property<string>("AnswerString")
+                    .HasMaxLength(255)
+                    .HasColumnType("nvarchar(255)");
+
+                b.Property<string>("DisplayText")
+                    .HasMaxLength(255)
+                    .HasColumnType("nvarchar(255)");
+
+                b.Property<string>("ExtCode")
+                    .HasMaxLength(100)
+                    .HasColumnType("nvarchar(100)");
+
+                b.Property<string>("ExtCodeSystem")
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
+
+                b.HasKey("Id");
+
+                b.HasIndex("AnswerListId");
+
+                b.ToTable("loinc_answer_list", "catalog");
+            });
+
+            modelBuilder.Entity("MedicalCatalogService.Models.LoincAnswerLink", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                b.Property<string>("AnswerListId")
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
+
+                b.Property<string>("ApplicableContext")
+                    .HasMaxLength(100)
+                    .HasColumnType("nvarchar(100)");
+
+                b.Property<string>("LinkType")
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
+
+                b.Property<string>("LoincNum")
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .HasColumnType("nvarchar(20)");
+
+                b.HasKey("Id");
+
+                b.HasIndex("LoincNum", "AnswerListId")
+                    .IsUnique();
+
+                b.ToTable("loinc_answer_link", "catalog");
+            });
+
+            modelBuilder.Entity("MedicalCatalogService.Models.LoincAlias", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                b.Property<string>("Alias")
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnType("nvarchar(255)");
+
+                b.Property<string>("LoincNum")
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .HasColumnType("nvarchar(20)");
+
+                b.HasKey("Id");
+
+                b.HasIndex("LoincNum", "Alias")
+                    .IsUnique();
+
+                b.ToTable("loinc_alias", "catalog");
+            });
+
+            modelBuilder.Entity("MedicalCatalogService.Models.LoincPanel", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                b.Property<string>("Name")
+                    .HasMaxLength(500)
+                    .HasColumnType("nvarchar(500)");
+
+                b.Property<string>("PanelLoincNum")
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .HasColumnType("nvarchar(20)");
+
+                b.HasKey("Id");
+
+                b.HasIndex("PanelLoincNum")
+                    .IsUnique();
+
+                b.ToTable("loinc_panel", "catalog");
+            });
+
+            modelBuilder.Entity("MedicalCatalogService.Models.LoincPanelItem", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                b.Property<string>("ItemLoincNum")
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .HasColumnType("nvarchar(20)");
+
+                b.Property<string>("Optionality")
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
+
+                b.Property<string>("PanelLoincNum")
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .HasColumnType("nvarchar(20)");
+
+                b.HasKey("Id");
+
+                b.HasIndex("PanelLoincNum", "ItemLoincNum")
+                    .IsUnique();
+
+                b.ToTable("loinc_panel_item", "catalog");
             });
 #pragma warning restore 612, 618
         }
